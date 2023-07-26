9f67ef8e86fa8c7c063d3c0bbb594eb55600f88f
Jay stones <Joelpaul345@gmail.com>
2023-07-26 01:49:44 +0100

    :white_check_mark:Complete Task 4

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -11,18 +11,20 @@ class LRUCache(BaseCaching):
         self.usage_order = []
 
     def put(self, key, item):
-        """Add an item in the cache using LRU algorithm"""
-        if key is not None and item is not None:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                if self.usage_order:
-                    # Get the least recently used item
-                    # (first item in the usage_order)
-                    lru_key = self.usage_order.pop(0)
-                    # Remove the least recently used item from the cache_data
-                    self.cache_data.pop(lru_key)
-                    print("DISCARD:", lru_key)
-            self.cache_data[key] = item
+        """put item into cache_data with LIFO algorithm
+        Args:
+            key ([type]): key of dictionary
+            item ([type]): item to insert in dictionary
+        """
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.usage_order:
+            discard = self.usage_order.pop(0)
+            del self.cache_data[discard]
+            print('DISCARD: {}'.format(discard))
+        if key and item:
+            if key in self.cache_data:
+                self.usage_order.remove(key)
             self.usage_order.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
         """Get an item by key using LRU algorithm"""
