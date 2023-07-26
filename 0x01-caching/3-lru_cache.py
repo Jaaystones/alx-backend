456b343de5975ecefa72951579d5a46fda6e4920
Jay stones <Joelpaul345@gmail.com>
2023-07-26 01:56:06 +0100

    :white_check_mark: complete task 5

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -5,10 +5,13 @@ from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
+    """LRUCache class
+    Args:
+        BaseCaching (class): Basic class for this class
+    """
     def __init__(self):
-        """Initialize LRUCache"""
         super().__init__()
-        self.usage_order = []
+        self.__keys = []
 
     def put(self, key, item):
         """put item into cache_data with LIFO algorithm
@@ -16,22 +19,23 @@ class LRUCache(BaseCaching):
             key ([type]): key of dictionary
             item ([type]): item to insert in dictionary
         """
-        if len(self.cache_data) == self.MAX_ITEMS and key not in self.usage_order:
-            discard = self.usage_order.pop(0)
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.__keys:
+            discard = self.__keys.pop(0)
             del self.cache_data[discard]
             print('DISCARD: {}'.format(discard))
         if key and item:
             if key in self.cache_data:
-                self.usage_order.remove(key)
-            self.usage_order.append(key)
+                self.__keys.remove(key)
+            self.__keys.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
-        """Get an item by key using LRU algorithm"""
-        if key is not None and key in self.cache_data:
-            # Move the accessed item to the end of the usage_order
-            # (most recently used)
-            self.usage_order.remove(key)
-            self.usage_order.append(key)
-            return self.cache_data[key]
-        return None
+        """get value of cache_data dictionary
+        Args:
+            key ([type]): key to search into cache_data
+        """
+        if not key or key not in self.cache_data:
+            return None
+        self.__keys.remove(key)
+        self.__keys.append(key)
+        return self.cache_data[key]
