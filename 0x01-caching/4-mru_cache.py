ac724cf240ea575d0d8209ebe09585fbd5537f58
Jay stones <Joelpaul345@gmail.com>
2023-07-26 01:57:15 +0100

    :white_check_mark:Complete Task 6

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -5,35 +5,36 @@ from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """Represents an object that allows storing and
-    retrieving items from a dictionary with an MRU
-    removal mechanism when the limit is reached.
+    """MRUCache class
+    Args:
+        BaseCaching (class): Basic class for this class
     """
     def __init__(self):
-        """Initialize MRUCache"""
         super().__init__()
-        self.usage_order = []
+        self.__keys = []
 
     def put(self, key, item):
-        """Add an item in the cache using MRU algorithm"""
-        if key is not None and item is not None:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                if self.usage_order:
-                    # Get the most recently used item
-                    # (last item in the usage_order)
-                    mru_key = self.usage_order.pop()
-                    # Remove the most recently used item from the cache_data
-                    self.cache_data.pop(mru_key)
-                    print("DISCARD:", mru_key)
+        """put item into cache_data with MRU algorithm
+        Args:
+            key ([type]): key of dictionary
+            item ([type]): item to insert in dictionary
+        """
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.__keys:
+            discard = self.__keys.pop()
+            del self.cache_data[discard]
+            print('DISCARD: {}'.format(discard))
+        if key and item:
+            if key not in self.__keys:
+                self.__keys.append(key)
             self.cache_data[key] = item
-            self.usage_order.append(key)
 
     def get(self, key):
-        """Get an item by key using MRU algorithm"""
-        if key is not None and key in self.cache_data:
-            # Move the accessed item to the end of the usage_order
-            # (most recently used)
-            self.usage_order.remove(key)
-            self.usage_order.append(key)
-            return self.cache_data[key]
-        return None
+        """get value of cache_data dictionary
+        Args:
+            key ([type]): key to search into cache_data
+        """
+        if not key or key not in self.cache_data:
+            return None
+        self.__keys.remove(key)
+        self.__keys.append(key)
+        return self.cache_data[key]
